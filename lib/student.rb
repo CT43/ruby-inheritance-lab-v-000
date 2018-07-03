f26803e690e41cbda9ebfaa8108111acdace06fb
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-03 23:18:33 +0000

    automatically backed up by learn

---
+++ lib/student.rb
@@ -0,0 +1,15 @@
+require_relative"./user.rb"
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
+end
